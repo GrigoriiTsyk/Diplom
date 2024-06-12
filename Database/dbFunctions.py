e8f59ab1488b3a3274da6d7551730d85c8b08ee1
Grigorii Tsykarev <gregstykarev@yandex.ru>
2024-06-12 20:13:47 +0300

    Предфинальная версия

--- Database/dbFunctions.py
+++ Database/dbFunctions.py
@@ -77,13 +77,67 @@ def add_patient(session, form):
     conn.commit()
     conn.close()
 
+def update_patient(form, id):
+    conn = sqlite3.connect('Database/database.db')
+    c = conn.cursor()
+
+    patient = c.execute('''
+        SELECT *
+        FROM patients p
+        WHERE p.id = ?
+    ''', (id,)).fetchone()
+
+    print(patient)
+    print(form)
+
+    if patient != None:
+        name = form.get('inputLastName') + " " + form.get('inputFirstName') + " " + form.get('inputMiddleName')
+
+        pasport_series = form.get('inputPasSeries')
+        pasport_number = form.get('inputPasNum')
+        snils = form.get('inputSnils')
+        diagnostic_image = form.get('inputImage')
+        diagnostic_result = form.get('inputResult')
+
+        print(diagnostic_image)
+        print(diagnostic_result)
+
+        c.execute('''
+            UPDATE patients 
+            SET name = ?, pasport_series = ?, pasport_number = ?, snils = ?
+            WHERE id = ?;
+        ''', (name, pasport_series, pasport_number, snils, id))
+
+        conn.commit()
+
+        if diagnostic_image != '':
+            c.execute('''
+                        UPDATE patients 
+                        SET diagnostic_image = ?
+                        WHERE id = ?;
+                    ''', (diagnostic_image, id))
+
+            conn.commit()
+
+        if diagnostic_result != '':
+            c.execute('''
+                        UPDATE patients 
+                        SET diagnostic_result = ?
+                        WHERE id = ?;
+                    ''', (diagnostic_result, id))
+
+            conn.commit()
+
+    conn.commit()
+    conn.close()
+
 def get_patients(user_id):
     conn = sqlite3.connect('Database/database.db')
     #conn = sqlite3.connect('database.db')
     c = conn.cursor()
 
     result = c.execute('''
-        SELECT p.name FROM users u
+        SELECT p.name, p.id FROM users u
         JOIN doctors d ON u.users_doctors = d.doctor_id 
         JOIN patients p ON d.patient = p.id 
         WHERE u.id = ? and d.doctor_id = u.users_doctors;
@@ -94,7 +148,159 @@ def get_patients(user_id):
 
     return result
 
+def get_patients_by_id(doctor_id):
+    conn = sqlite3.connect('Database/database.db')
+    #conn = sqlite3.connect('database.db')
+    c = conn.cursor()
+
+    result = c.execute('''
+        SELECT p.name, p.id FROM doctors d
+        JOIN patients p ON d.patient = p.id
+        WHERE d.doctor_id = ?;
+    ''', doctor_id).fetchall()
+
+    conn.commit()
+    conn.close()
+
+    return result
+
+def get_all_doctors():
+    conn = sqlite3.connect('Database/database.db')
+    #conn = sqlite3.connect('database.db')
+    c = conn.cursor()
+
+    result = c.execute('''
+        SELECT DISTINCT doctor_id, name FROM doctors;
+    ''').fetchall()
+
+    conn.commit()
+    conn.close()
+
+    return result
+
+def get_patient_info_from_db(id):
+    conn = sqlite3.connect('Database/database.db')
+    # conn = sqlite3.connect('database.db')
+    c = conn.cursor()
+
+    result = c.execute('''
+           SELECT * FROM patients
+           WHERE id = ?
+       ''', (id,)).fetchall()
+
+    conn.commit()
+    conn.close()
+
+    return result
+
+def get_patient_image_path(id):
+    conn = sqlite3.connect('Database/database.db')
+    #conn = sqlite3.connect('database.db')
+    c = conn.cursor()
+
+    result = c.execute('''
+               SELECT p.diagnostic_image FROM patients p 
+               WHERE id = ?;
+           ''', (id,)).fetchall()
+
+    conn.commit()
+    conn.close()
+
+    return result
+
+def get_patient_diagnostic_result(id):
+    conn = sqlite3.connect('Database/database.db')
+    # conn = sqlite3.connect('database.db')
+    c = conn.cursor()
+
+    result = c.execute('''
+               SELECT p.diagnostic_result FROM patients p 
+               WHERE id = ?
+           ''', (id,)).fetchall()
+
+    conn.commit()
+    conn.close()
+
+    return result
+
+def set_patient_image_path(imageName, id):
+    conn = sqlite3.connect('Database/database.db')
+    # conn = sqlite3.connect('database.db')
+    c = conn.cursor()
+
+    result = c.execute('''
+                   UPDATE patients 
+                    SET diagnostic_image = ?
+                    WHERE id = ?;
+               ''', (imageName, id)).fetchall()
+
+    conn.commit()
+    conn.close()
+
+    return result
+
+def set_patient_diagnostic_result(diagnostic_result, id):
+    conn = sqlite3.connect('Database/database.db')
+    # conn = sqlite3.connect('database.db')
+    c = conn.cursor()
+
+    c.execute('''
+       UPDATE patients 
+        SET diagnostic_result = ?
+        WHERE id = ?;
+    ''', (diagnostic_result, id)).fetchall()
+
+    conn.commit()
+    conn.close()
+
+def del_doctor(id):
+    conn = sqlite3.connect('Database/database.db')
+    # conn = sqlite3.connect('database.db')
+    c = conn.cursor()
+
+    c.execute('''
+           DELETE FROM patients
+           WHERE doctor = ?;
+       ''', id)
+
+    conn.commit()
+
+    c.execute('''
+        DELETE FROM doctors
+        WHERE doctor_id = ?;
+    ''', id)
+
+    conn.commit()
+
+    conn.close()
+
+def del_patient(id):
+    conn = sqlite3.connect('Database/database.db')
+    #conn = sqlite3.connect('database.db')
+    c = conn.cursor()
+
+    c.execute('''
+               DELETE FROM patients
+               WHERE id = ?;
+           ''', id)
+
+    conn.commit()
+
+    c.execute('''
+            DELETE FROM doctors
+            WHERE patient = ?;
+        ''', id)
+
+    conn.commit()
+
+    conn.close()
+
 #create_user(0, 'admin', 'your_password')
 # create_user(1, 'doctor', 'doctor')
 #add_foreign_key()
 #print(get_patients('1'))
+#print(get_all_doctors()[0][1])
+
+#del_patient(str(6))
+
+#get_patient_image_path(str(13))
--- main.py
+++ main.py
@@ -1,26 +1,56 @@
+# coding: utf8
+import tensorflow as tf
 from flask import Flask, session
 from flask import render_template, redirect, url_for, request
 import sqlite3
 import hashlib
+import os
+import numpy as np
+from werkzeug.utils import secure_filename
 
-import tensorflow as tf
-from keras.src.saving import load_model
-
-from Database.dbFunctions import create_user, get_patients, add_patient
+from Database.dbFunctions import create_user, get_patients, add_patient, get_patient_info_from_db, \
+    get_patient_diagnostic_result, set_patient_image_path, get_patient_image_path, get_all_doctors, get_patients_by_id, \
+    del_doctor, del_patient, update_patient
 
 name = 'main'
+UPLOAD_FOLDER = 'static/images/screens'
+
+ALLOWED_EXTENSIONS = {'png'}
 
 app = Flask(name)
 app.secret_key = 'e80141c41b762bf063e91cb7ae7ca26f3617e8aa'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 def get_db_connection():
     conn = sqlite3.connect('Database/database.db')
     conn.row_factory = sqlite3.Row
     return conn
 
-def load_keras_model():
-    global model
-    model = load_model('Neuro/classify_model.h5')
+# def load_keras_model():
+#     global model
+#     model = tf.keras.models.load_model('Neuro/saved_model_2.h5')
+
+def predict_image(image_path):
+    img = tf.keras.preprocessing.image.load_img(image_path, target_size=(224, 224))
+    img_array = tf.keras.preprocessing.image.img_to_array(img)
+    img_array = img_array / 255.0
+    img_array = np.expand_dims(img_array, axis=0)
+
+    print(img_array)
+
+    model = tf.keras.models.load_model('Neuro/saved_model_2.h5')
+
+    model.summary()
+
+    prediction = model.predict(img_array)
+
+    print(prediction[0])
+
+    return prediction
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route('/')
 def home():
@@ -36,6 +66,7 @@ def admin_login():
 
         conn = get_db_connection()
         user = conn.execute('SELECT * FROM users WHERE username = ?', (username,)).fetchone()
+        session['gUsername'] = username
         global gUsername
         gUsername = username
         conn.close()
@@ -53,9 +84,36 @@ def admin_login():
 
     return render_template('Login page.html', error=error)
 
-@app.route('/admin_panel')
+@app.route('/admin_panel', methods=['GET', 'POST'])
 def admin_panel():
-    return render_template('Admin panel.html', username=gUsername)
+    doctorsList = get_all_doctors()
+
+    print(doctorsList)
+    print(doctorsList[0])
+
+    return render_template('Admin panel.html', username=session['gUsername'], doctorsList=doctorsList)
+
+@app.route('/del_doctor/<int:id>')
+def delete_doctor(id):
+    del_doctor(str(id))
+
+    return redirect(url_for('admin_panel'))
+
+@app.route('/edit_doctor/<int:doctor_id>/del_patient/<int:id>')
+def delete_patient(id, doctor_id):
+    del_patient(str(id))
+
+    print(request.url)
+
+    return redirect(url_for('edit_doctor', id=doctor_id))
+
+@app.route('/doctor_main/del_patient/<int:id>')
+def doctor_delete_patient(id, doctor_id):
+    del_patient(str(id))
+
+    print(request.url)
+
+    return redirect(url_for('doctor_main'))
 
 @app.route('/logout')
 def logout():
@@ -99,7 +157,96 @@ def doctor_main_page():
 
     return render_template('Doctor main page.html', patients_list=list)
 
+@app.route('/edit_doctor/<int:id>', methods=['GET', 'POST'])
+def edit_doctor(id):
+    if request.method =='POST':
+        add_patient(session=session, form=request.form)
+
+    list = get_patients_by_id(str(id))
+
+    return render_template('Edit doctor page.html', patients_list=list, doctor_id=id)
+
+@app.route('/edit_patient/<int:id>', methods=['GET', 'POST'])
+def edit_patient(id):
+    image_path = None
+
+    query = get_patient_image_path(str(id))
+
+    diagnostic_result = get_patient_diagnostic_result(str(id))
+
+    if (query[0][0] == None):
+        diagnostic_image = False
+    else:
+        diagnostic_image = True
+        image_path = '/' + UPLOAD_FOLDER + '/' + query[0][0]
+
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            print('Не могу прочитать файл')
+            return redirect(request.url)
+
+        file = request.files['file']
+
+        if file.filename == '':
+            print('Нет выбранного файла')
+            return redirect(request.url)
+
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            print(filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            old_path = image_path
+            image_path = '/' + UPLOAD_FOLDER + '/' + filename
+
+            set_patient_image_path(filename, id)
+
+    print('image_path: ')
+    print(image_path)
+    print('diagnostic_result: ')
+    print(diagnostic_result[0][0])
+
+    return render_template('Edit patient page.html', id=id, diagnostic_image=diagnostic_image, image_path=image_path, diagnostic_result=diagnostic_result[0][0])
+
+@app.route('/patient_info/<int:id>', methods=['GET', 'POST'])
+def get_patient_info(id):
+    list = get_patient_info_from_db(str(id))
+
+    print(list[0])
+
+    return render_template('Patient info.html', item=list[0])
+
+@app.route('/add_diagnostic/<int:id>', methods=['POST'])
+def add_diagnostic(id):
+    print('Функция диагностики')
+
+    query = get_patient_image_path(str(id))
+
+    if(query[0][0] != None):
+        image_name = get_patient_image_path(str(id))[0][0]
+
+        image_path = UPLOAD_FOLDER + '/' + image_name
+
+        print( "Путь до изображения: " + image_path)
+
+        if(os.path.exists(image_path)):
+            print("файл существует")
+
+            predict_image(image_path)
+        else:
+            print("Файл не существует")
+    else:
+        print('Нет изображения для предсказания')
+
+    return redirect('/edit_patient/' + str(id))
+
+@app.route('/update_pateint_info/<int:id>', methods=['POST'])
+def update_patient_info(id):
+    if request.method == 'POST':
+        update_patient(form=request.form, id=str(id))
+
+    return redirect('/patient_info/' + str(id))
+
 if name == 'main':
-    load_keras_model()
+    #load_keras_model()
 
     app.run(debug=True)
